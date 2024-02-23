369e9db9fd48b1b31d78c4ee32e3890a0ad50813
gonzalezzfelipe <gonzalezz_felipe@hotmail.com>
2024-02-23 15:47:39 -0300

    chore: Convert bootstrap to module

---
+++ bootstrap/variables.tf
@@ -0,0 +1,75 @@
+variable "namespace" {
+  type = string
+}
+
+variable "dns_zone" {
+  type    = string
+  default = "demeter.run"
+}
+
+variable "extension_name" {
+  type    = string
+  default = "submitapi-m1"
+}
+
+variable "ingress_class" {
+  type    = string
+  default = "submitapi-m1"
+}
+
+variable "networks" {
+  type    = list(string)
+  default = ["mainnet", "preprod", "preview"]
+}
+
+// Operator
+variable "operator_image_tag" {
+  type = string
+}
+
+variable "api_key_salt" {
+  type    = string
+  default = "submitapi-salt"
+}
+
+variable "dcu_per_request" {
+  type = map(string)
+  default = {
+    "mainnet"   = "10"
+    "preprod"   = "5"
+    "preview"   = "5"
+    "sanchonet" = "5"
+  }
+}
+
+variable "metrics_delay" {
+  type    = number
+  default = 60
+}
+
+// Gateway
+variable "gateway_replicas" {
+  type    = number
+  default = 1
+}
+
+variable "instances" {
+  type = map(object({
+    image            = string
+    salt             = string
+    node_private_dns = string
+    testnet_magic    = number
+    network          = string
+    replicas         = option(number)
+    resources = option(object({
+      limits = object({
+        cpu    = string
+        memory = string
+      })
+      requests = object({
+        cpu    = string
+        memory = string
+      })
+    }))
+  }))
+}
