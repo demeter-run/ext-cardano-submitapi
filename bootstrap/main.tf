f6a6113bed57e05d7cad02a8d20a3ba4bd5277a2
Felipe Gonzalez <gonzalezz_felipe@hotmail.com>
2024-03-12 15:12:32 -0300

    chore: Convert bootstrap to module (#7)
    * chore: Convert bootstrap to module
    
    * Minor fix in var usage
    
    * Fix image variable on instances
    
    * Remove api_key_salt default

---
+++ bootstrap/main.tf
@@ -0,0 +1,70 @@
+resource "kubernetes_namespace" "namespace" {
+  metadata {
+    name = var.namespace
+  }
+}
+
+module "submitapi_v1_feature" {
+  depends_on         = [kubernetes_namespace.namespace]
+  source             = "./feature"
+  namespace          = var.namespace
+  operator_image_tag = var.operator_image_tag
+  metrics_delay      = var.metrics_delay
+  ingress_class      = var.ingress_class
+  dns_zone           = var.dns_zone
+  api_key_salt       = var.api_key_salt
+  dcu_per_request    = var.dcu_per_request
+}
+
+module "submitapi_v1_gateway" {
+  depends_on     = [kubernetes_namespace.namespace]
+  source         = "./gateway"
+  namespace      = var.namespace
+  replicas       = var.gateway_replicas
+  dns_zone       = var.dns_zone
+  networks       = var.networks
+  extension_name = var.extension_name
+}
+
+module "submitapi_configs" {
+  depends_on = [kubernetes_namespace.namespace]
+  for_each   = { for network in var.networks : "${network}" => network }
+
+  source    = "./configs"
+  namespace = var.namespace
+  network   = each.value
+}
+
+module "submitapi_instances" {
+  depends_on = [kubernetes_namespace.namespace, module.submitapi_configs]
+  for_each   = var.instances
+  source     = "./instance"
+
+  namespace        = var.namespace
+  image            = each.value.image
+  salt             = each.value.salt
+  node_private_dns = each.value.node_private_dns
+  testnet_magic    = each.value.testnet_magic
+  network          = each.value.network
+  replicas         = coalesce(each.value.replicas, 1)
+  resources = coalesce(each.value.resources, {
+    limits : {
+      cpu : "200m",
+      memory : "1Gi"
+    }
+    requests : {
+      cpu : "200m",
+      memory : "500Mi"
+    }
+  })
+}
+
+module "submitapi_services" {
+  depends_on = [kubernetes_namespace.namespace]
+  for_each   = { for network in var.networks : "${network}" => network }
+  source     = "./service"
+
+  namespace = var.namespace
+  network   = each.value.network
+}
+
