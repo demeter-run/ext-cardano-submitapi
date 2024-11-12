2b899700eaa753fc85bb3bb76d0193badd1bf7f3
Paulo Bressan <paulo.bressan@outlook.com>
2024-11-12 19:46:18 -0300

    fix: adjusted host regex (#39)

--- proxy/src/proxy.rs
+++ proxy/src/proxy.rs
@@ -22,7 +22,7 @@ pub struct SubmitApiProxy {
 }
 impl SubmitApiProxy {
     pub fn new(state: Arc<State>, config: Arc<Config>) -> Self {
-        let host_regex = Regex::new(r"(dmtr_[\w\d-]+)?\.?.+").unwrap();
+        let host_regex = Regex::new(r"([dmtr_]?[\w\d-]+)?\.?.+").unwrap();
 
         Self {
             state,
