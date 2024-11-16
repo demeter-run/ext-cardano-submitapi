3b66e521ef4bd22f26f523a27fe8d802c38b386a
paulobressan <paulo.bressan@outlook.com>
2024-11-16 16:40:21 -0300

    fix: fixed proxy api key

--- proxy/src/proxy.rs
+++ proxy/src/proxy.rs
@@ -82,13 +82,12 @@ impl SubmitApiProxy {
             .unwrap();
 
         let captures = self.host_regex.captures(host).unwrap();
-        let mut key = session
+        let key = session
             .get_header(DMTR_API_KEY)
-            .map(|v| v.to_str().unwrap())
+            .and_then(|v| v.to_str().ok())
+            .or_else(|| captures.get(1).map(|v| v.as_str()))
             .unwrap_or_default();
-        if let Some(m) = captures.get(1) {
-            key = m.as_str();
-        }
+
         key.to_string()
     }
 
